96083d44686edb5377faf9ea7a9e36d03f5d7bf4
MegMcC <116989994+Meg-Hinckley35@users.noreply.github.com>
2023-05-05 10:18:59 -0400

    fetch location and stored location functions

--- weather.js
+++ weather.js
@@ -14,9 +14,9 @@ const app = {
         let countryCode = document.getElementById("country-input").value;
         let key = "a65653b1059d6cc607d60a89f12cd23c";
         let limit = 1;
-        let url = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`;
+        let url = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode},${countryCode}&limit=${limit}&appid=${key}`;
 
-        //fetching weather data from our url
+        //fetching location data from our url
         if (url) {
             fetch(url)
               .then((resp) => {
@@ -31,6 +31,30 @@ const app = {
               })
               .catch(console.err);
           }
-        },        
+        },
+        
+        //fetch function to grab location data that has been stored already
+        fetchStoredLocation: (storage) => {
+            let storageCity = storage.city;
+            let storageCountry = storage.country;
+            let key = "a65653b1059d6cc607d60a89f12cd23c";
+            let limit = 1;
+            let storageUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${storageCity},${storageCountry}&limit=${limit}&appid=${key}`;
+        
+            if (storageUrl) {
+              fetch(storageUrl)
+                .then((resp) => {
+                  if (!resp.ok) throw new Error(resp.statusText);
+                  return resp.json();
+                })
+                .then((data) => {
+                  let usableStorageData = {};
+                  usableStorageData.lat = data[0].lat;
+                  usableStorageData.lon = data[0].lon;
+                  app.fetchWeather(usableStorageData);
+                })
+                .catch(console.err);
+            }
+          },
 
 }
